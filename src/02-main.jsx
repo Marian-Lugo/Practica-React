e2ca3b997a607d5dfd5dd06c5ea3b07ddbbaa12a
Marian Estigarribia <mnelugo@gmail.com>
2023-12-20 16:41:55 -0300

    Proyecto creado con Vite

---
+++ src/02-main.jsx
@@ -0,0 +1,14 @@
+//import React from 'react'
+import ReactDOM from 'react-dom/client'
+//import App from './App.jsx'
+import './index.css'
+//import React from 'react'
+
+//const h1 = React.createElement('div', null, React.createElement('ul' , null, React.createElement('li', null, 'item1')));
+const h1 =<div><ul><li>Hola Mundo</li></ul></div>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+  h1
+)
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,3 @@
+export function HelloWorld(){
+    return <div>Practicando react</div>
+}
